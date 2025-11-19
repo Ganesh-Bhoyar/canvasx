4e9fa87dc9d1eb9ca71d9b5f5124e2cc8d6c8cbe
Ganesh-Bhoyar <ganeshbhoyar4808h@gmail.com>
2025-11-19 15:48:21 +0530

    updated routes

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_URL = "https://canvasx-http.onrender.com//api/v1";
+export const HTTP_URL = "https://canvasx-http.onrender.com/api/v1";
 export const WS_URL = "wss://canvasx-ws.onrender.com";
